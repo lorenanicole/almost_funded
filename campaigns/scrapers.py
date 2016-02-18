a9a794384c6f4c153768cf609f3d8dc657f59daf
Lorena Mesa <me@lorenamesa.com>
2016-02-18 10:36:11 -0600

    Add base logic for finding Give Forward campaigns by query that are almost funded

--- campaigns/scrapers.py
+++ campaigns/scrapers.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from bs4 import BeautifulSoup
 
 
 class KickstarterScraper(object):
@@ -18,3 +19,31 @@ class KickstarterScraper(object):
             print content
 
         return content
+
+
+class GiveForwardScraper(object):
+    base_url = "http://www.giveforward.com/"
+    fundraiser_query_path = "fundraisers?query={0}"
+
+    @classmethod
+    def find_projects(cls, query):
+        response = requests.get(cls.base_url + cls.fundraiser_query_path.format(query))
+        html = BeautifulSoup(response.content)
+        # button = html.find('input', {'id': 'search_filters_Close To Goal'})
+
+        campaigns = html.find_all('div', class_='fr-card-search')
+        almost_funded = []
+
+        for indx, campaign in enumerate(campaigns):
+            percent_raised = float(campaign.find('span', class_='meter').get('style').strip('width:').strip('%'))
+            if percent_raised > 90.0 and percent_raised != 100.0:
+                almost_funded.append(campaign)
+
+        return almost_funded
+
+
+# if __name__ == '__main__':
+#     scraper = GiveForwardScraper
+#     campaigns = scraper.find_projects('cancer')
+#     for campaign in campaigns:
+#         print float(campaign.find('span', class_='meter').get('style').strip('width:').strip('%'))
