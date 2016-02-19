d7e7f0c3b9eb4332396820d13ef34d06998f853b
Lorena Mesa <me@lorenamesa.com>
2016-02-19 17:53:21 -0600

    Add GiveForward and GoFundMe scrapers

--- campaigns/scrapers.py
+++ campaigns/scrapers.py
@@ -1,3 +1,4 @@
+from types import NoneType
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -7,8 +8,8 @@ class KickstarterScraper(object):
     # TODO: get list of all categories from projects for rendering possible list on main view
 
 
-    base_url = "https://www.kickstarter.com/"
-    projects_query_path = "projects/search.json?search={0}&term={1}"
+    base_url = "https://www.kickstarter.com"
+    projects_query_path = "/projects/search.json?search={0}&term={1}"
 
     @classmethod
     def scrape_projects(cls, search, term):
@@ -22,8 +23,8 @@ class KickstarterScraper(object):
 
 
 class GiveForwardScraper(object):
-    base_url = "http://www.giveforward.com/"
-    fundraiser_query_path = "fundraisers?query={0}"
+    base_url = "http://www.giveforward.com"
+    fundraiser_query_path = "/fundraisers?query={0}"
 
     @classmethod
     def find_projects(cls, query):
@@ -31,19 +32,80 @@ class GiveForwardScraper(object):
         html = BeautifulSoup(response.content)
         # button = html.find('input', {'id': 'search_filters_Close To Goal'})
 
-        campaigns = html.find_all('div', class_='fr-card-search')
+        projects = html.find_all('div', class_='fr-card-search')
         almost_funded = []
 
-        for indx, campaign in enumerate(campaigns):
-            percent_raised = float(campaign.find('span', class_='meter').get('style').strip('width:').strip('%'))
-            if percent_raised > 90.0 and percent_raised != 100.0:
-                almost_funded.append(campaign)
+        for project in projects:
+            percent_raised = float(project.find('span', class_='meter').get('style').strip('width:').strip('%'))
+            if 90.0 < percent_raised < 100.0:
+                almost_funded.append(project)
+
+        return almost_funded
+
+class GoFundMeScraper(object):
+    base_url = "https://www.gofundme.com"
+    fundraiser_query_path = "/mvc.php?route=search&term={0}"
+    almost_there_path = "/mvc.php?route=index/hometoggle&toggle=almostthere"
+
+    @classmethod
+    def find_projects(cls, query, paginate=False):
+        response = requests.get(cls.base_url + cls.fundraiser_query_path.format(query))
+        html = BeautifulSoup(response.content)
+        projects = html.find_all('div', class_='search_tile')
+
+        if paginate:
+
+            try:
+                next_page_path = html.find('a', class_='box').get('href')
+                num_projects = html.find('div', class_='numbers').get_text()
+                more_projects = True
+            except AttributeError as e:
+                more_projects = False
+                num_projects = ''
+
+
+
+            if 'thousands' in num_projects:
+                reasonable_limit = 20
+            else:
+                reasonable_limit = 40
+
+            while more_projects and reasonable_limit >= 0:
+                response = requests.get(cls.base_url + next_page_path)
+                html = BeautifulSoup(response.content)
+                new_projects = html.find_all('div', class_='search_tile')
+                projects += new_projects
+
+                try:
+                    next_page_path = html.find('a', class_='box').get('href')
+                    num_projects = html.find('div', class_='numbers').get_text()
+                    more_projects = True
+                except AttributeError as e:
+                    more_projects = False
+
+                reasonable_limit -= 1
+                print reasonable_limit, len(projects)
+
+        almost_funded = []
+
+        for project in projects:
+            percent_raised = int(project.find('span', class_='fill').get('style').strip('width: ').strip('%;'))
+            print percent_raised
+            if 90.0 < percent_raised < 100.0:
+                almost_funded.append(project)
 
         return almost_funded
 
 
-# if __name__ == '__main__':
-#     scraper = GiveForwardScraper
-#     campaigns = scraper.find_projects('cancer')
-#     for campaign in campaigns:
-#         print float(campaign.find('span', class_='meter').get('style').strip('width:').strip('%'))
+
+if __name__ == '__main__':
+    # scraper = GiveForwardScraper
+    # campaigns = scraper.find_projects('cancer')
+    # for campaign in campaigns:
+    #     print float(campaign.find('span', class_='meter').get('style').strip('width:').strip('%'))
+
+    # scraper = GoFundMeScraper
+    # projects = scraper.find_projects('cancer', True)
+    #
+    # for project in projects:
+    #     print "{0} percent raised".format(int(project.find('span', class_='fill').get('style').strip('width: ').strip('%;')))
