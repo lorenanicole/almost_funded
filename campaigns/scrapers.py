54977fcb0e43b0a045c4accdd3bdbdaa54cc41d5
Lorena Mesa <me@lorenamesa.com>
2016-02-20 09:49:14 -0600

    Fix CrowdRise scraper logic to crawl additional pages

--- campaigns/scrapers.py
+++ campaigns/scrapers.py
@@ -105,9 +105,10 @@ class CrowdRiseScraper(object):
 
             try:
                 next_page_url = html.find('a', class_='page_link').get('href')
-                print next_page_url
+
                 other_project_paths = html.find_all('a', class_='page_link')
                 other_project_paths = filter(lambda path: path.get_text() == 'Next >', other_project_paths)
+
                 if other_project_paths:
                     more_projects = True
                 else:
@@ -118,19 +119,20 @@ class CrowdRiseScraper(object):
 
             reasonable_limit = 40
 
-            while more_projects and reasonable_limit >= 0:
+            while more_projects and reasonable_limit >= 1:
                 response = requests.get(next_page_url)
                 html = BeautifulSoup(response.content)
-                new_projects = html.find_all('div', class_='search_tile')  # TODO: This is still buggy fix meeeee
+                new_projects = html.find_all('div', class_='content clearfix ')  # TODO: This is still buggy fix meeeee
                 projects += new_projects
 
                 try:
-                    next_page_path = html.find('a', class_='page_link').get('href')
-
                     other_project_paths = html.find_all('a', class_='page_link')
                     other_project_paths = filter(lambda path: path.get_text() == 'Next >', other_project_paths)
+
                     if other_project_paths:
                         more_projects = True
+                        next_page_url = other_project_paths[0].get('href')
+
                     else:
                         more_projects = False
 
@@ -139,24 +141,21 @@ class CrowdRiseScraper(object):
 
                 reasonable_limit -= 1
 
-                print reasonable_limit, len(projects)
-
         almost_funded = []
 
         for project in projects:
             description = project.find('div', class_='searchResultsStory').get_text()
-            print description
+            # print description
             budget_items = list(re.findall('\$\d{1,}', description))
             budget_items = map(lambda item: int(item.strip('$')), budget_items)
 
             try:
                 percent_raised = budget_items[0] / float(budget_items[1])
-                print percent_raised
-                if 90.0 < percent_raised < 100.0:
+
+                if 0.90 < percent_raised < 1.0:
                     almost_funded.append(project)
             except ZeroDivisionError as e:
                 percent_raised = 0.0
-
 
         return almost_funded
 
