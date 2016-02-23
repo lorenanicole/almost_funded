11335521b091baa5ab4b16e1079b0294b8d3e485
Lorena Mesa <me@lorenamesa.com>
2016-02-23 17:24:57 -0600

    Update Kickstarter task to handle dedup / update logic

--- campaigns/tasks.py
+++ campaigns/tasks.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from bulk_update.helper import bulk_update
 
 from celery.schedules import crontab
 from celery.decorators import periodic_task
@@ -22,15 +23,27 @@ def task_scrape_latest_kickstarter():
     """
     Saves latest almost funded projects from Kickstarter
     """
+    # TODO: Dedup logic ensuring unique projects added - http://stackoverflow.com/questions/17311059/django-bulk-create-ignore-duplicates
+    # TODO: Use Q objects? http://stackoverflow.com/questions/24502658/django-get-or-create-performance-optimization-for-a-list-of-objects
+
     KICKSTARTER_CATEGORIES = ['art', 'comics', 'crafts', 'dance', 'design', 'fashion', 'film & video', 'food', 'games', 'journalism', 'music', 'photography', 'publishing', 'technology', 'theater']
     projects = []
     scraper = KickstarterScraper
 
     last_updated = datetime.utcnow()
+    project_urls = set([])
 
     for category in KICKSTARTER_CATEGORIES:
         logger.info("Kickstarter processing category " + category)
         projects += scraper.find_projects(category, paginate=True)
+
+    project_data_map = {p.get('urls', {}).get('web', {}).get('project', 'N/A'): p for p in projects}
+
+    project_urls |= project_data_map.keys()
+
+    existing = list(Campaign.objects.filter(url__in=project_urls))  # .values_list('id', flat=True)
+
+    projects = filter(lambda p: p.get('urls', {}).get('web', {}).get('project', 'N/A') not in existing, projects)
 
     projects = map(lambda p: {'name': p.get('name'),
                                 'deadline': datetime.utcfromtimestamp(p.get('deadline')),
@@ -44,7 +57,18 @@ def task_scrape_latest_kickstarter():
                                 'location': p.get('location', {}).get('short_name', 'N/A'),
                                 'category': Category.objects.get_or_create(category=p.get('category', {}).get('slug', 'N/A'))[0]}, projects)
 
-    projects = map(lambda p: Campaign(**p), projects)
     logger.info("Kickstarter scraper - Bulk creating")
+
+    projects = map(lambda p: Campaign(**p), projects)
     Campaign.objects.bulk_create(projects)
+
+    logger.info("Kickstarter scraper - Bulk update")
+
+    for project in existing:
+        project.goal = project_data_map[project.url].get('goal')
+        project.last_updated = last_updated
+        project.deadline = datetime.utcfromtimestamp(project_data_map[project.url].get('deadline'))
+
+    bulk_update(existing)
+
     logger.info("Saved latest almost funded projects from Kickstarter")
