2ce3d1677ccc40a33ae49c80d1f131ffa1320a25
Lorena Mesa <me@lorenamesa.com>
2016-03-05 16:07:32 -0600

    Set static root

--- almostfunded/settings.py
+++ almostfunded/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Celery Config
 
 BROKER_URL = 'redis://localhost:6379'
